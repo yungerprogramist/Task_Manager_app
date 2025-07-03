38053ee5ab9a248cdc093686968c103a11b7e65e
yungerprogramist <alekseiviskovath228@mail.ru>
2025-07-03 20:18:28 +0500

    first commit

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
+}
